30980b6c9ac2bdbaa6d71e78637b2c886583eae9
king-zhangbn <60092461+king-zhangbn@users.noreply.github.com>
2021-08-17 09:20:18 +0800

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -10,4 +10,4 @@ git add -A
 git commit -m 'deploy'
 
 # 推到你仓库的 master 分支
-git push -f git@github.com:zhuzhaohua/zhuzhaohua.github.io.git master
+git push -f git@github.com:king-zhangbn/king-zhangbn.github.io.git master
